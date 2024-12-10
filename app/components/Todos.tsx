632ad382160a8fa074fe15349d74e106dee8f5ea
Rupert Smith <rupertwebdev@gmail.com>
2024-12-10 10:19:23 +0000

    Deleted

--- app/components/Todos.tsx
+++ app/components/Todos.tsx
@@ -7,82 +7,81 @@ export default function Todos() {
   const [image, setImage] = useState<File | null>(null);
   const [imagePreview, setImagePreview] = useState<string | null>(null);
 
+  // const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (event.target.files && event.target.files[0]) {
+  //     const file = event.target.files[0];
+  //     setImage(file);
+
+  //     const imageUrl = URL.createObjectURL(file);
+  //     setImagePreview(imageUrl);
+  //   }
+  // };
+
+  // const handleSubmitImage = () => {
+  //   if (!image) {
+  //     alert("Please select an image to upload");
+  //     return;
+  //   }
+
+  //   const formData = new FormData();
+  //   formData.append("image", image);
+
+  //   console.log(formData);
+
+  //   axios
+  //     .post(
+  //       "http://localhost:8080/todos",
+  //       {
+  //         image,
+  //       },
+  //       {
+  //         headers: {
+  //           "Content-Type": "multipart/form-data",
+  //         },
+  //       }
+  //     )
+  //     .then((response) => {
+  //       console.log("Image uploaded successfully:", response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error uploading image:", error);
+  //     });
+  // };
+
   const handleGetXML = () => {
     axios({
-      headers: { accept: "application/xml" },
       method: "get",
       url: "https://mocktarget.apigee.net/xml",
     }).then((res) => {
       if (res.headers["content-type"].includes("application/xml")) {
-        const jsonData = xml2json(res.data);
-        console.log(`This was converted from XML! ${jsonData}`);
+        const convertedData = xml2json(res.data);
+        console.log(convertedData);
       }
       if (res.headers["content-type"].includes("application/json")) {
-        console.log(`This is just json! ${res.data}`);
+        console.log(res.data);
       }
     });
-  };
-
-  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files[0]) {
-      const file = event.target.files[0];
-      setImage(file);
-
-      const imageUrl = URL.createObjectURL(file);
-      setImagePreview(imageUrl);
-    }
-  };
-
-  const handleSubmitImage = () => {
-    if (!image) {
-      alert("Please select an image to upload");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("image", image);
-
-    console.log(formData);
-
-    axios
-      .post(
-        "http://localhost:8080/todos",
-        {
-          image,
-        },
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      )
-      .then((response) => {
-        console.log("Image uploaded successfully:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error uploading image:", error);
-      });
   };
 
   return (
     <div>
       <div>
-        <input
+        {/* <input
           type="file"
           accept="image/*"
           onChange={handleImageChange}
           style={{ marginBottom: "10px" }}
-        />
-        <button onClick={handleSubmitImage}>Submit Image</button>
+        /> */}
+        {/* <button onClick={handleSubmitImage}>Submit Image</button> */}
         <button onClick={handleGetXML}>Get XML</button>
       </div>
-      {imagePreview && (
+      {/* {imagePreview && (
         <img
           style={{ width: "300px" }}
           src={imagePreview}
           alt="Image preview"
         />
-      )}
+      )} */}
     </div>
   );
 }
