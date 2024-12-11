e0d69f6d2bee43f3b3e714813e916f308ee2eda1
Rupert Smith <rupertwebdev@gmail.com>
2024-12-11 15:15:58 +0000

    Delete

--- app/components/Todos.tsx
+++ app/components/Todos.tsx
@@ -4,5 +4,24 @@ import axios from "axios";
 import { xml2json } from "xml-js";
 
 export default function Todos() {
-  return <div>Test</div>;
+  const handleGetXml = () => {
+    axios({
+      method: "get",
+      url: "https://mocktarget.apigee.net/xml",
+    }).then((res) => {
+      if (res?.headers["content-type"]?.includes("application/xml")) {
+        console.log(xml2json(res.data));
+      }
+
+      if (res?.headers["content-type"]?.includes("application/json")) {
+        console.log(res);
+      }
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleGetXml}>get XML</button>
+    </div>
+  );
 }
